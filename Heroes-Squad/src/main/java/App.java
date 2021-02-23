7d56cd23acd8742c2d9ed5d387847ca8361c6ce0
BarakaIan <ianb.makale@gmail.com>
2021-02-23 17:11:40 +0300

    add a pom.xml file

--- Heroes-Squad/src/main/java/App.java
+++ Heroes-Squad/src/main/java/App.java
@@ -12,18 +12,18 @@ import java.util.Map;
 import static spark.Spark. *;
 
 public class App {
-//    static int getHerokuAssignedPort() {
-//        staticFileLocation("/public");
-//        ProcessBuilder processBuilder = new ProcessBuilder();
-//        if (processBuilder.environment().get("PORT") != null) {
-//            return Integer.parseInt(processBuilder.environment().get("PORT"));
-//        }
-//        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-//    }
+    static int getHerokuAssignedPort() {
+        staticFileLocation("/public");
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 
     public static void main(String[] args) {
 
-//        port(getHerokuAssignedPort());
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
 
